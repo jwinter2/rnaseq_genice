78c07e4aba34d422c6343db245f23c5a151cec29
jwinter2 <62965499+jwinter2@users.noreply.github.com>
2026-03-23 11:10:13 -0700

    Added assembly file

---
+++ spades_cmo_24.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+## Job Name
+#SBATCH --account=def-nagissa
+#SBATCH --job-name=spades_CMO2024_tx
+#SBATCH --array=1-132%30
+#SBATCH --time=05:00:00
+#SBATCH --mem=250G
+#SBATCH --cpus-per-task=32
+#SBATCH --mail-user=jwinter2@uw.edu
+#SBATCH --mail-type=ALL
+
+#load spades
+
+module load spades
+
+#get list of samples for job
+sample=$(sed -n "${SLURM_ARRAY_TASK_ID}p" samplenames.txt)
+
+spades.py --rna -1 ${sample}_1_out_paired.fq.gz -2 ${sample}_2_out_paired.fq.gz -o spades_out_${sample}
