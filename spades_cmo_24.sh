57928666818bf92ddcba3e71fbd92b305b0a3633
jwinter2 <62965499+jwinter2@users.noreply.github.com>
2026-04-02 09:24:01 -0700

    Update spades_cmo_24.sh

--- spades_cmo_24.sh
+++ spades_cmo_24.sh
@@ -2,7 +2,7 @@
 ## Job Name
 #SBATCH --account=def-nagissa
 #SBATCH --job-name=spades_CMO2024_tx
-#SBATCH --array=1-132%30
+#SBATCH --array=1-66%30
 #SBATCH --time=05:00:00
 #SBATCH --mem=250G
 #SBATCH --cpus-per-task=32
@@ -16,4 +16,4 @@ module load spades
 #get list of samples for job
 sample=$(sed -n "${SLURM_ARRAY_TASK_ID}p" samplenames.txt)
 
-spades.py --rna -1 ${sample}_1_out_paired.fq.gz -2 ${sample}_2_out_paired.fq.gz -o spades_out_${sample}
+spades.py --rna -1 clean_fastq/normalized/${sample}_norm_1.fastq.gz -2 clean_fastq/normalized/${sample}_norm_2.fastq.gz -o spades_single_assembly/spades_out_${sample}
